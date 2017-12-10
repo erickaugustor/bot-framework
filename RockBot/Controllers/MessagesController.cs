8cee56134acc67ae0b376e61e34736e46d493ef5
erick.augustor <erick.augustoramos@gmail.com>
2017-12-10 20:14:06 -0200

    version 0.0.10 - LUIS dialog not correct :(

--- RockBot/Controllers/MessagesController.cs
+++ RockBot/Controllers/MessagesController.cs
@@ -25,13 +25,16 @@ namespace RockBot
                 // Antigo-novo: await Conversation.SendAsync(activity, () => new Dialogs.DialogStudy());
                 // await Conversation.SendAsync(activity, () => new Dialogs.LUISDialog());
 
-                //bool bSetStock = false;
+                bool bSetStock = false;
                 StockLUIS stLuis = await LUISStockClient.ParseUserInput(activity.Text);
                 string strRet = string.Empty;
                 string strStock = activity.Text;
 
-
+             
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+                StateClient _stateClient = activity.GetStateClient();
+                BotData _botData = _stateClient.BotState.GetUserData(activity.ChannelId, activity.Conversation.Id);
 
                 switch (stLuis.intents[0].intent)
                 {
@@ -45,6 +48,12 @@ namespace RockBot
                         strRet = "I don't have a previous stock to look up!222222";
                         break;
 
+                }
+
+                if (bSetStock)
+                {
+                    _botData.SetProperty<string>("LastStock", stLuis.entities[0].entity);
+                    _stateClient.BotState.SetUserData(activity.ChannelId, activity.Conversation.Id, _botData);
                 }
 
                 Activity reply = activity.CreateReply(strRet);
