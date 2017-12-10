762ec5c4078e578a921a86a6e73d5b1a16c4c654
erick.augustor <erick.augustoramos@gmail.com>
2017-12-10 18:22:57 -0200

    version 0.0.5 - LUIS part3

--- RockBot/Services/LUIS.cs
+++ RockBot/Services/LUIS.cs
@@ -1,27 +1,59 @@
-﻿using System.IO;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RockBot.Services
 {
+    public class LUISStockClient
+    {
+
+
+        public static async Task<StockLUIS> ParseUserInput(string strInput)
+        {
+            string strRet = string.Empty;
+            string strEscaped = Uri.EscapeDataString(strInput);
+
+            using (var client = new HttpClient())
+            {
+                string uri = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/50e1e801-a55c-4087-9e49-77f78680e2c8?subscription-key=2d3842b38b4f4ed4a570c01c03f8ca88&timezoneOffset=0&q=" + strEscaped;
+                HttpResponseMessage msg = await client.GetAsync(uri);
+
+                if (msg.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await msg.Content.ReadAsStringAsync();
+                    var _Data = JsonConvert.DeserializeObject<StockLUIS>(jsonResponse);
+                    return _Data;
+                }
+            }
+            return null;
+        }
+    }
+
+    public class StockLUIS
+    {
+        public string query { get; set; }
+        public lTopscoringintent[] intents { get; set; }
+        public lEntity[] entities { get; set; }
+    }
 
     public class Rootobject
     {
         public string query { get; set; }
-        public Topscoringintent topScoringIntent { get; set; }
-        public Entity[] entities { get; set; }
+        public lTopscoringintent topScoringIntent { get; set; }
+        public lEntity[] entities { get; set; }
     }
 
-    public class Topscoringintent
+    public class lTopscoringintent
     {
         public string intent { get; set; }
         public float score { get; set; }
     }
 
-    public class Entity
+    public class lEntity
     {
         public string entity { get; set; }
         public string type { get; set; }
