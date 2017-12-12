d4494f30aed338c2b3680ee4e287717a29362b06
erick.augustor <erick.augustoramos@gmail.com>
2017-12-12 17:25:29 -0200

    version 0.0.14 - dialogs test one

--- RockBot/Dialogs/DialogStudyI.cs
+++ RockBot/Dialogs/DialogStudyI.cs
@@ -27,7 +27,43 @@ namespace RockBot.Dialogs
 
             await context.PostAsync($"Ola!\n Voce quer um número RANDOM? Sua IDADE? Um ELOGIO?");
 
-            context.Wait(MsgRecebidaComeco);
+            context.Wait(MsgRecebidaEscolha);
+            //texto do usuario
+        }
+
+        //intercepta a msg do usuario
+
+        private async Task MsgRecebidaEscolha(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var msg = await argument;
+            //pega argumento do usuario
+
+            //ifs do usuario
+            if (msg.Text.ToLower().Equals("random", StringComparison.InvariantCultureIgnoreCase))
+            {
+                await context.PostAsync("Informe um número inteiro mínimo:");
+                context.Wait(MsgRecebidaRand1);
+            }
+            else if (msg.Text.ToLower().Equals("idade", StringComparison.InvariantCultureIgnoreCase))
+            {
+                await context.PostAsync("Informe sua data de nascimento:");
+                context.Wait(MsgRecebidaData);
+            }
+            else if (msg.Text.ToLower().Equals("escolha", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Random rand = new Random();
+                var pos = rand.Next(1, 4);
+                var listaElogios = new string[] { "O rodrigo é um lixo? Sim!", "O rodrigo é muito lixo", "Rodrigo lixão" };
+                await context.PostAsync(listaElogios[pos]);
+            }
+            else
+            {
+                await context.PostAsync("Desculpe, não entendi!");
+                //volta para o inicio
+                context.Wait(MsgRecebidaComeco);
+            }
+
+
         }
     }
 }
